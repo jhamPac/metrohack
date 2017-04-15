9e9c04e903f0154b58fd9d99e822a418c7bdc6dd
Cloud <kevia@triangle.host>
2017-04-15 00:31:04 -0700

    Got em coach

--- public/javascripts/call.js
+++ public/javascripts/call.js
@@ -1,7 +1,7 @@
 (function (window) {
   'use strict';
 
-  const TRAILS = {
+  const ADA = {
     esriURL: 'https://services1.arcgis.com/WGzzp37bqYMLyzDR/arcgis/rest/services/AGOLHackathon1/FeatureServer/5/query?where=1%3D1&outFields=OBJECTID,CLASS,TYPE,STYLE,CONDITION,LOCATION,Shape__Length&outSR=4326&f=json',
     parks: [],
     flattenedParks: [],
@@ -9,7 +9,7 @@
   };
 
   $.ajax({
-    url: TRAILS.esriURL
+    url: ADA.esriURL
   })
   .done(function(res) {
     var result = JSON.parse(res);
@@ -23,7 +23,7 @@
 
       if (trailClass == 5 && trailCondition == 1 && trailStyle == "WALKING") {
 
-        TRAILS.parks.push(trail);
+        ADA.parks.push(trail);
       }
 
     });
@@ -33,9 +33,40 @@
     console.log('Error: ' + err.status);
   });
 
+  // Wait a split second for ajax call
+  setTimeout(function () {
 
+    ADA.parks.forEach(function (sourcePark) {
 
-  // Sticking to Global for now
-  window.PARKS = TRAILS;
+      if (!ADA.flattenedParks.some(function (targetPark) { return targetPark.location == sourcePark.attributes.LOCATION  })) {
+
+        // Get location to use as key
+        var key = sourcePark.attributes.LOCATION;
+
+        // Delete property so we just have trails
+        delete sourcePark.attributes.LOCATION;
+
+        var parkObj = {location: key};
+        parkObj.trails = [];
+        parkObj.trails.push(sourcePark.attributes);
+        ADA.flattenedParks.push(parkObj);
+
+      } else {
+
+        var targetPark = ADA.flattenedParks.filter(function(tpark) { return tpark.location == sourcePark.attributes.LOCATION })[0];
+        delete sourcePark.attributes.LOCATION;
+
+        targetPark.trails.push(sourcePark.attributes);
+
+      }
+
+    });
+
+      console.log(ADA);
+
+      // Set this to global window for now; I know its nasty
+      window.PARKS = ADA;
+
+  }, 1000);
 
 })(window);
