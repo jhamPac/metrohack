0f87db5090656af95fb2567fcb7789cfefe36047
Cloud <kevia@triangle.host>
2017-04-15 01:37:02 -0700

    describe the problem

--- public/javascripts/call.js
+++ public/javascripts/call.js
@@ -4,7 +4,7 @@
   const ADA = {
     esriURL: 'https://services1.arcgis.com/WGzzp37bqYMLyzDR/arcgis/rest/services/AGOLHackathon1/FeatureServer/5/query?where=1%3D1&outFields=OBJECTID,CLASS,TYPE,STYLE,CONDITION,LOCATION,Shape__Length&outSR=4326&f=json',
     parks: [],
-    flattenedParks: [],
+    collatedParks: [],
     count: 0
   };
 
@@ -21,6 +21,7 @@
       var trailCondition = trail.attributes.CONDITION;
       var trailStyle = trail.attributes.STYLE;
 
+      // Check against criteria if it passes ADA standars
       if (trailClass == 5 && trailCondition == 1 && trailStyle == "WALKING") {
 
         ADA.parks.push(trail);
@@ -38,22 +39,27 @@
 
     ADA.parks.forEach(function (sourcePark) {
 
-      if (!ADA.flattenedParks.some(function (targetPark) { return targetPark.location == sourcePark.attributes.LOCATION  })) {
+      var PARKS_NAME = sourcePark.attributes.LOCATION;
+
+      if (!ADA.collatedParks.some(function (targetPark) { return targetPark.location == PARKS_NAME  })) {
 
         // Get location to use as key
-        var key = sourcePark.attributes.LOCATION;
+        var key = PARKS_NAME;
 
         // Delete property so we just have trails
         delete sourcePark.attributes.LOCATION;
 
+        // Create
         var parkObj = {location: key};
         parkObj.trails = [];
         parkObj.trails.push(sourcePark.attributes);
-        ADA.flattenedParks.push(parkObj);
+
+        
+        ADA.collatedParks.push(parkObj);
 
       } else {
 
-        var targetPark = ADA.flattenedParks.filter(function(tpark) { return tpark.location == sourcePark.attributes.LOCATION })[0];
+        var targetPark = ADA.collatedParks.filter(function(tpark) { return tpark.location == sourcePark.attributes.LOCATION })[0];
         delete sourcePark.attributes.LOCATION;
 
         targetPark.trails.push(sourcePark.attributes);
