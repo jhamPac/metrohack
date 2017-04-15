8aa6d906092197de4d21e8f0882bd9c85e3d4c3c
Cloud <kevia@triangle.host>
2017-04-14 22:51:52 -0700

    really?

--- public/javascripts/call.js
+++ public/javascripts/call.js
@@ -1,35 +1,41 @@
-(function () {
+(function (window) {
   'use strict';
 
-  var adaSafeTrails = new Array();
-  var count = 0;
+  const TRAILS = {
+    esriURL: 'https://services1.arcgis.com/WGzzp37bqYMLyzDR/arcgis/rest/services/AGOLHackathon1/FeatureServer/5/query?where=1%3D1&outFields=OBJECTID,CLASS,TYPE,STYLE,CONDITION,LOCATION,Shape__Length&outSR=4326&f=json',
+    parks: [],
+    flattenedParks: [],
+    count: 0
+  };
 
   $.ajax({
-    url: 'https://services1.arcgis.com/WGzzp37bqYMLyzDR/arcgis/rest/services/AGOLHackathon1/FeatureServer/5/query?where=1%3D1&outFields=OBJECTID,CLASS,TYPE,STYLE,CONDITION,LOCATION,Shape__Length&outSR=4326&f=json'
+    url: TRAILS.esriURL
   })
-.done(function(res) {
-  var result = JSON.parse(res);
-  var allTrails = result['features'];
+  .done(function(res) {
+    var result = JSON.parse(res);
+    var allTrails = result['features'];
 
-  allTrails.forEach(function(trail) {
+    allTrails.forEach(function(trail) {
 
-    var trailClass = trail.attributes.CLASS;
-    var trailCondition = trail.attributes.CONDITION;
-    var trailStyle = trail.attributes.STYLE;
+      var trailClass = trail.attributes.CLASS;
+      var trailCondition = trail.attributes.CONDITION;
+      var trailStyle = trail.attributes.STYLE;
 
-    if (trailClass == 5 && trailCondition == 1 && trailStyle == "WALKING") {
-      adaSafeTrails.push(trail);
-      ++count;
-    }
+      if (trailClass == 5 && trailCondition == 1 && trailStyle == "WALKING") {
 
+        TRAILS.parks.push(trail);
+      }
+
+    });
+
+  })
+  .fail(function(err) {
+    console.log('Error: ' + err.status);
   });
 
-  console.log(count);
-  console.log(adaSafeTrails[0]);
 
-})
-.fail(function(err) {
-  console.log('Error: ' + err.status);
-});
 
-})();
+  // Sticking to Global for now
+  window.PARKS = TRAILS;
+
+})(window);
